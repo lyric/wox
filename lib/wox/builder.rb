edca33c429872be48dd20ee4cefdddd978d2def2
Lyric Hartley <lhartley@salesforce.com>
2013-10-03 13:58:31 -0700

    switch to xctool

--- lib/wox/builder.rb
+++ lib/wox/builder.rb
@@ -9,7 +9,7 @@ module Wox
 
       log_file = File.join environment[:build_dir], "build-#{configuration}.log"
 
-      run_command "xcodebuild #{environment[:build_selector].to_s} #{environment[:target_selector].to_s} -configuration #{configuration} clean build OBJROOT=#{environment[:build_dir]} SYMROOT=#{environment[:build_dir]}", :results => log_file
+      run_command "xctool #{environment[:build_selector].to_s} #{environment[:target_selector].to_s} -configuration #{configuration} clean build OBJROOT=#{environment[:build_dir]} SYMROOT=#{environment[:build_dir]}", :results => log_file
     end
   end
 end
