0a29e8e1255c570a4e0526e1232c0e8a2038a5cb
Lyric Hartley <lhartley@salesforce.com>
2013-10-09 21:41:55 -0700

    hard code sdk for now

--- lib/wox/builder.rb
+++ lib/wox/builder.rb
@@ -9,7 +9,7 @@ module Wox
 
       log_file = File.join environment[:build_dir], "build-#{configuration}.log"
 
-      command = "xctool #{environment[:build_selector].to_s} #{environment[:target_selector].to_s} -sdk #{sdk} -configuration #{configuration} clean build"
+      command = "xctool #{environment[:build_selector].to_s} #{environment[:target_selector].to_s} -sdk iphoneos -configuration #{configuration} clean build"
       puts command
       run_command command, :results => log_file
     end
